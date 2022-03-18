6e0287356338e8e3d33ba7ca9aad627c8dbff1a0
dianarod28 <201880207@psu.palawan.edu.ph>
2022-03-18 20:50:43 +0800

    changes made

--- LogApp/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	define('ROOT_URL', 'index.php');
-	define('DB_HOST', 'sql6.freesqldatabase.com');
-	define('DB_USER', 'sql6479680');
-	define('DB_PASS', '7H12eGeIUg');
-	define('DB_NAME', 'sql6479680');
-
---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+	define('ROOT_URL', 'index.php');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6479680');
+	define('DB_PASS', '7H12eGeIUg');
+	define('DB_NAME', 'sql6479680');
+
