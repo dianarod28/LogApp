63f01f0e8c8d551701c46ac19d7763cec054b0fd
dianarod28 <201880207@psu.palawan.edu.ph>
2022-03-18 13:33:25 +0800

    Using freesqldatabase

--- LogApp/config/config.php
+++ LogApp/config/config.php
@@ -1,6 +1,7 @@
 <?php
 	define('ROOT_URL', 'index.php');
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', '');
-	define('DB_NAME', 'logapp');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6479680');
+	define('DB_PASS', '7H12eGeIUg');
+	define('DB_NAME', 'sql6479680');
+
